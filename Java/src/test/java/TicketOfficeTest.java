9a71eaf8e45d633ed57026f4f10cd0964096ad4b
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-07-14 21:41:44 +0100

    chore: the test is now passing.

--- Java/src/test/java/TicketOfficeTest.java
+++ Java/src/test/java/TicketOfficeTest.java
@@ -2,11 +2,8 @@ import org.junit.*;
 
 public class TicketOfficeTest {
 
-    /**
-     * Change the 5 to a 4 to get the test passing.
-     */
     @Test
     public void testFailingHookup() {
-        Assert.assertEquals(5, 2 + 2);
+        Assert.assertEquals(4, 2 + 2);
     }
 }
