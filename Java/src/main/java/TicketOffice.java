d77eb1c51ad16f853c1dfcb63fad164a11206fcd
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-11-06 22:53:23 +0000

    refactor: extracted a computation to an intention-revealing method.

--- Java/src/main/java/TicketOffice.java
+++ Java/src/main/java/TicketOffice.java
@@ -15,7 +15,8 @@ public class TicketOffice {
             List<Seat> availableSeats = trainDataService.availableSeatsOn(request.trainId);
             String bookingReference = availableSeats.isEmpty() ? "" : bookingReferenceGenerator.generate();
 
-            trainDataService.reserve(request.trainId, availableSeats.stream().map(Seat::number).toArray(String[]::new), bookingReference);
+            String[] seatNumbers = availableSeats.stream().map(Seat::number).toArray(String[]::new);
+            trainDataService.reserve(request.trainId, seatNumbers, bookingReference);
             return new Reservation(request.trainId, availableSeats, bookingReference);
         } else {
             return Reservation.none(request.trainId);
