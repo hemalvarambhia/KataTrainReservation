1e979c94d4883199bf8624f5b45840ce10303b77
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-11-07 22:51:04 +0000

    refactor: removed some duplicate code to an intention-revealing variable.

--- Java/src/main/java/TicketOffice.java
+++ Java/src/main/java/TicketOffice.java
@@ -11,15 +11,16 @@ public class TicketOffice {
     }
     
     public Reservation makeReservation(ReservationRequest request) {
+        String train = request.trainId;
         if(request.seatCount >= 1) {
-            List<Seat> availableSeats = trainDataService.availableSeatsOn(request.trainId);
+            List<Seat> availableSeats = trainDataService.availableSeatsOn(train);
             if(availableSeats.isEmpty()) {
-                return Reservation.none(request.trainId);
+                return Reservation.none(train);
             }
 
             String bookingReference = bookingReferenceGenerator.generate();
             String[] seatNumbers = availableSeats.stream().map(Seat::number).toArray(String[]::new);
-            trainDataService.reserve(request.trainId, seatNumbers, bookingReference);
+            trainDataService.reserve(train, seatNumbers, bookingReference);
             return new Reservation(request.trainId, availableSeats, bookingReference);
         } else {
             return Reservation.none(request.trainId);
