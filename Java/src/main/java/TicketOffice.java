af29169f7731bfc30642444027d3e10658ef31c5
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-11-17 20:03:49 +0000

    refactor: made the no reservation use case a guard clause.

--- Java/src/main/java/TicketOffice.java
+++ Java/src/main/java/TicketOffice.java
@@ -14,18 +14,18 @@ public class TicketOffice {
     
     public Reservation makeReservation(ReservationRequest request) {
         String train = request.trainId;
-        if(request.seatCount >= 1) {
-            List<Seat> availableSeats = trainDataService.availableSeatsOn(train);
-            if(availableSeats.isEmpty()) {
-                return Reservation.none(train);
-            }
-            List<Seat> seatsToBook =
-                    availableSeats.stream().limit(request.seatCount).collect(Collectors.toList());
-            String bookingReference = reserveSeatsOn(train, seatsToBook);
-            return new Reservation(request.trainId, seatsToBook, bookingReference);
-        } else {
+
+        if(request.seatCount == 0) {
             return Reservation.none(request.trainId);
         }
+
+        List<Seat> availableSeats = trainDataService.availableSeatsOn(train);
+        if(availableSeats.isEmpty()) {
+            return Reservation.none(train);
+        }
+        List<Seat> seatsToBook = availableSeats.stream().limit(request.seatCount).collect(Collectors.toList());
+        String bookingReference = reserveSeatsOn(train, seatsToBook);
+        return new Reservation(request.trainId, seatsToBook, bookingReference);
     }
 
     private String reserveSeatsOn(String train, List<Seat> seatsToReserve) {
