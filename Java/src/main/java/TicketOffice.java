450d524d03b670d04acdfc18befdf2f3ec1fff4f
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-11-17 20:04:35 +0000

    refactor: improved code formatting.

--- Java/src/main/java/TicketOffice.java
+++ Java/src/main/java/TicketOffice.java
@@ -15,14 +15,11 @@ public class TicketOffice {
     public Reservation makeReservation(ReservationRequest request) {
         String train = request.trainId;
 
-        if(request.seatCount == 0) {
-            return Reservation.none(request.trainId);
-        }
+        if(request.seatCount == 0) { return Reservation.none(request.trainId); }
 
         List<Seat> availableSeats = trainDataService.availableSeatsOn(train);
-        if(availableSeats.isEmpty()) {
-            return Reservation.none(train);
-        }
+        if(availableSeats.isEmpty()) { return Reservation.none(train); }
+
         List<Seat> seatsToBook = availableSeats.stream().limit(request.seatCount).collect(Collectors.toList());
         String bookingReference = reserveSeatsOn(train, seatsToBook);
         return new Reservation(request.trainId, seatsToBook, bookingReference);
