7e8685d99d911ef82a67dba2edc48e974eee65ac
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-11-29 20:27:38 +0000

    refactor: copied some code to a more sensible place.

--- Java/src/main/java/Seat.java
+++ Java/src/main/java/Seat.java
@@ -1,7 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Seat {
     public final String coach;
     public final int seatNumber;
+
+    public static List<Seat> seats(String... seatNumbers) {
+        return Arrays.stream(seatNumbers)
+                .map(Seat::from)
+                .collect(Collectors.toList());
+    }
+
+    private static Seat from(String seat) {
+        String coach = Character.toString(seat.charAt(0));
+        int seatNumber = Character.getNumericValue(seat.charAt(1));
+        return new Seat(coach, seatNumber);
+    }
 
     public Seat(String coach, int seatNumber) {
         this.coach = coach;
