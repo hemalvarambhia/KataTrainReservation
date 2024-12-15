5c8696a7847810eec6e3debd5089622cc0574504
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-15 21:59:06 +0000

    refactor: removed obsolete code.

--- Java/src/main/java/TrainDataService.java
+++ Java/src/main/java/TrainDataService.java
@@ -2,7 +2,6 @@ import java.util.List;
 
 public interface TrainDataService {
     List<Seat> availableSeatsOn(String train);
-    Coach coaches(String train);
     boolean reserve(String train, String[] seats, String bookingReference);
     Coach firstCoach(String train);
 }
