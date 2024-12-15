32a54c828e35b40fca175e4af09acaf14610d069
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-15 21:31:26 +0000

    refactor: introduced the concept of a coach.

---
+++ Java/src/main/java/Coach.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coach {
+    private final List<Seat> seats;
+
+    public Coach(List<Seat> seats) {
+        this.seats = seats;
+    }
+
+    public List<Seat> getSeats() {
+        return new ArrayList<>(seats);
+    }
+}
